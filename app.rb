0b3fd5fbc499b53fcc5ed0cef8adfcb36efb6842
Justin <justinms@outlook.com>
2020-03-15 11:34:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Justin!"
+  end
+
+  get '/hometown' do
+    "My hometown is Houston, TX"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Keep your head up' by Andy Grammer"
+  end
 end
