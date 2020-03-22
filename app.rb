0a5f9b1b4f260ccc35c89fbec2a1daba17b90c9a
Jade Lynn Masker <hopefulfire@gmail.com>
2020-03-22 11:48:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Jade"
+	end
+
+	get '/hometown' do
+		"My hometown is Vassar"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Vibrasphere--Breathing Place"
+	end
+
 end
